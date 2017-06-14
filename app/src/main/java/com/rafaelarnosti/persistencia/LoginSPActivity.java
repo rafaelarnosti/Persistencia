a1b2c63182bcb228e18c76eb85299c23f7835927
logonrm <logonrm@fiap.com.br>
2017-06-14 20:01:34 -0300

    Initial commit

---
+++ app/src/main/java/com/rafaelarnosti/persistencia/LoginSPActivity.java
@@ -0,0 +1,13 @@
+package com.rafaelarnosti.persistencia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginSPActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_sp);
+    }
+}
